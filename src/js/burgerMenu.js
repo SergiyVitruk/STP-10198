c772a1b51bd0c2798e469c8dd747b80cb20f4618
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-08-24 00:00:14 +0300

    modal done

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,11 +1,28 @@
 const openBtnEl = document.querySelector('[data-action="open"]');
 const closeBtnEl = document.querySelector('[data-action="close"]');
 const burgerMenuEl = document.querySelector('[data-visible]');
+const burgerLinks = burgerMenuEl.querySelectorAll('a');
 
-openBtnEl.addEventListener('click', e => {
+function openMenu() {
   burgerMenuEl.dataset.visible = 'open';
+}
+function closeMenu() {
+  burgerMenuEl.dataset.visible = 'close';
+}
+
+openBtnEl.addEventListener('click', openMenu);
+closeBtnEl.addEventListener('click', closeMenu);
+
+burgerLinks.forEach(link => {
+  link.addEventListener('click', closeMenu);
 });
 
-closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
+document.addEventListener('click', e => {
+  if (
+    burgerMenuEl.dataset.visible === 'open' &&
+    !burgerMenuEl.contains(e.target) &&
+    !openBtnEl.contains(e.target)
+  ) {
+    closeMenu();
+  }
 });
